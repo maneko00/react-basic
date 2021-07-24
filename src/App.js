544adcaebe916fde6eec40d21486721e87ee9687
initial commit <>
2021-07-25 08:18:22 +0900

    コンポーネントのprops

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Article from './components/Article';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Article
+      title={'新・日本一わかりやすいReact入門'}
+      content={'今日のトピックはpropsについて'}
+      />
     </div>
   );
 }
