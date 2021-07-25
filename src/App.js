8a381364dfed6d79850cdc44ded4d8ff69e25bb6
initial commit <>
2021-07-25 12:12:27 +0900

    useEffect内でAPIを呼び出す

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import {Counter, BadCounter, TextInput, ToggleButton} from './components';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [name, setName] = useState('')
+  const [id, setId] = useState('maneko00')
+  const ids = ['maneko00', 'gaearon', 'aws', 'google', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setName(data.name)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  },[id])
+
   return (
     <div>
-      {/* <TextInput/> */}
-      {/* <Counter/> */}
-      {/* <BadCounter/> */}
-      <ToggleButton/>
+      <p>{id}の、GitHub上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
     </div>
   );
 }
