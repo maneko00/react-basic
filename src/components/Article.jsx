e8e97efb8de0737f08d3d0f4512e4b079a6c4b1a
initial commit <>
2021-07-25 10:11:53 +0900

    stateをpropsに渡す

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,16 @@
 import {useState} from 'react';
-// import React, { useState } from 'react';
-import {Content, Title} from './index';
+import {Content,Publishbutton, Title} from './index';
 
 const Article = (props) => {
     const [isPublished, setIsPublished] = useState(false)
-    console.log(isPublished)
-
+    const PublishArticle = () => {
+        setIsPublished(true)
+    }
     return (
         <div>
             <Title title={props.title} />
             <Content content={props.content} />
-            <button onClick={() => setIsPublished(true)}>公開</button>
+            <Publishbutton isPublished={isPublished} onClick={PublishArticle}/>
         </div>
     );
 };
