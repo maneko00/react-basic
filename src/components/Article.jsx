26c5144c9c31885fb58cfd09b64cd58599148ed4
initial commit <>
2021-07-25 09:51:21 +0900

    useStateの使い方

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,16 @@
+import {useState} from 'react';
+// import React, { useState } from 'react';
 import {Content, Title} from './index';
 
 const Article = (props) => {
+    const [isPublished, setIsPublished] = useState(false)
+    console.log(isPublished)
+
     return (
         <div>
             <Title title={props.title} />
             <Content content={props.content} />
+            <button onClick={() => setIsPublished(true)}>公開</button>
         </div>
     );
 };
